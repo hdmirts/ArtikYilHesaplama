7ae339b3903740adf0cdd44c8b53d83da3b6ca69
hatice demirtas <98237665+hdmirts@users.noreply.github.com>
2022-05-20 02:14:41 +0300

    Create artikYil.java

---
+++ artikYil.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class artikYil {
+    public static void main(String[] args) {
+        int yil;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl Giriniz :");
+        yil = input.nextInt();
+
+        if ((yil % 100 == 0) && (yil % 400 == 0)) {
+
+            System.out.println(yil + " Bir Artık Yıldır");
+
+        } else if ((yil % 4 == 0) && (yil % 100 != 0) && (yil % 400 != 0)) {
+
+            System.out.println(yil + " Bir Artık Yıldır");
+
+        } else {
+
+            System.out.println(yil + " Artık Yıl Değildir");
+        }
+
+    }
+}
